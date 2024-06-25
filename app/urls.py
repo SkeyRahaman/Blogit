c990358f0d2559fcea7aa9371a3283b4894618b0
Shakib <sakibmondal7@gmail.com>
2024-06-25 22:37:45 +0530

    day_13

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home_page'),
     path('post/<slug:slug>', views.post_page, name = 'post_page'), 
-    path('tag/<slug:slug>', views.tag, name='tag')   
+    path('tag/<slug:slug>', views.tag, name='tag_page'),
+    path('author/<slug:slug>', views.author, name='author_page'),
+    path('search/', views.search_posts, name='search_page'),
+    path('all_post/', views.all_post, name='all_post_page'),
+    path('about/', views.about, name='about_page'),
+    path('contact/', views.contact_us, name='contact_page'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from .models import Post,Tag, Comments
-from .forms import CommentForm
+from .models import Post,Tag, Comments, Profile, WebsiteMeta
+from .forms import CommentForm,SubscribeForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.db.models import Count
 
 # Create your views here.
 def post_page(request, slug):
@@ -38,11 +40,99 @@ def post_page(request, slug):
     return render(request, 'app/post.html', context)
 
 def index(request):
-    posts = Post.objects.all()
-    context = {'posts':posts}
+    top_post = Post.objects.all().order_by('-view_count')[:3]
+    recent_post = Post.objects.all().order_by('-last_updated')[:3] 
+    featured_post = Post.objects.filter(is_featured = True)
+    if featured_post:
+        featured_post = featured_post[0]
+    subscribe_form = SubscribeForm()
+    subscribe_successful = None
+
+    if request.POST:
+        subscribe_form = SubscribeForm(request.POST)
+        if subscribe_form.is_valid:
+            subscribe_form.save()
+            subscribe_successful = 'Subscribe Successfully'
+            subscribe_form = SubscribeForm()
+
+    website_info =  None
+    if WebsiteMeta.objects.all().exists():
+        website_info = WebsiteMeta.objects.all()[0]
+
+    context = {
+        'website_info' : website_info,
+        'top_post' : top_post,
+        'recent_post' : recent_post,
+        'featured_post' : featured_post,
+        'subscribe_form' : subscribe_form,
+        'subscribe_successful': subscribe_successful
+    }
     return render(request, 'app/index.html', context)
 
 def tag(request, slug):
-    posts = Tag.objects.get(slug = slug)
-    context = {'posts':posts}
-    return render(request, 'app/index.html', context)
+    tag = Tag.objects.get(slug = slug)
+    top_posts = Post.objects.filter(tags__in=[tag.id]).order_by('-view_count')
+    if top_posts:top_posts[:2]
+    recent_posts = Post.objects.filter(tags__in=[tag.id]).order_by('-last_updated')
+    if recent_posts:recent_posts[:3]
+    featured_posts = Post.objects.filter(is_featured = True)
+    if featured_posts:featured_posts[:3]
+    context = {
+        'tag':tag,
+        'tags':Tag.objects.all(),
+        'top_posts' : top_posts,
+        'recent_posts' : recent_posts,
+        'featured_posts' : featured_posts
+    }
+    return render(request, 'app/tag.html', context)
+
+def author(request, slug):
+    author = Profile.objects.get(slug = slug)
+    top_posts = Post.objects.filter(author=author.user).order_by('-view_count')
+    if top_posts:top_posts[:2]
+    recent_posts = Post.objects.filter(author=author.user).order_by('-last_updated')
+    if recent_posts:recent_posts[:3]
+    featured_posts = Post.objects.filter(is_featured = True)
+    if featured_posts:featured_posts[:3]
+    top_authors = User.objects.annotate(number=Count('post')).order_by('-number')
+    if top_authors:top_authors[:10]
+    context = {
+        'author' : author,
+        'top_authors' : top_authors,
+        'top_posts' : top_posts,
+        'recent_posts' : recent_posts,
+        'featured_posts' : featured_posts
+    }
+    return render(request, 'app/author.html', context)
+
+def search_posts(request):
+    search_query = ''
+    if request.GET.get('q'):
+        search_query = request.GET.get('q')
+    posts = Post.objects.filter(title__icontains=search_query)
+    context = {
+        'posts' : posts,
+        'search_query' : search_query,
+    }
+    return render(request, 'app/search.html', context)
+
+def all_post(request):
+    context = {
+        'data' : 1,
+    }
+    return render(request, 'app/all_post.html', context)
+
+def about(request):
+    website_info =  None
+    if WebsiteMeta.objects.all().exists():
+        website_info = WebsiteMeta.objects.all()[0]
+    context = {
+        'website_info' : website_info,
+    }
+    return render(request, 'app/about.html', context)
+
+def contact_us(request):
+    context = {
+        'data' : 1,
+    }
+    return render(request, 'app/contact_us.html', context)
